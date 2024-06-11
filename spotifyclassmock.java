3e783f78852483b51ca0282202ff44785f7b6ff2
echoxxkeeper_ <147575793+echoxxkeeper@users.noreply.github.com>
2024-06-11 12:50:46 +0800

    Update spotifyclassmock.java

--- spotifyclassmock.java
+++ spotifyclassmock.java
@@ -1,13 +1,8 @@
-// to do: make the four panel on for loop and inside is nested loop for the playlist panels that contains the album/singer picture, title/name, author...
-
-    //pps four outer panel is already done, only needs to place the for loop for label and for loop for inside panels...
-
 // make the content panel and its content adjusts when the sidebar panel has been expanded...
 
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -15,6 +10,8 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Point;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
@@ -24,33 +21,58 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-class SuperMain{
+class SuperMain implements ActionListener{
 
-    Frame frame = new Frame(null);
-
-    SuperMain(){
+    Data data = new Data();
+    private Frame frame;
+    private Panel sidebarPanel;
+    private JScrollPane scroll;
+    private Button[] object;
+    private Button libraryButton;
+    private Panel contentPanel;
+    private Panel[] listHolder;
+    Panel subTopPanel;
+    private Boolean is_toggled = false;
 
     final int arc = 20;
     // format as follows: posX, posY, width, height
-    Integer[] header_panel = {0, -5, 825, 70};
+
+    private Integer[] header_panel = {0, -5, 825, 70};
         //format as follows: posX, posY, width, height
         // Integer[] searchbar_value = {155, 20, 400, 40};
         //format as follows: posX, posY, width, height
-        Integer[] minimize_value = {650, 5, 90, 65};
+        private Integer[] minimize_value = {650, 5, 90, 65};
         //format as follows: posX, posY, width, height
-        Integer[] close_value = {735, 5, 90, 65};
+        private Integer[] close_value = {735, 5, 90, 65};
 
-    // format as follows: posX, posY, width, height,JScrollPane posX, JScrollPane posY JScrollPane width, JScrollPane height, arcX, arcY.
-    Integer[] side_bar_panel = {10, 80, 80, 650, 5, 5, 70, 630, 20, 20};
-    // format as follows: posX, posY, width, height, JScrollPane width, JScrollPane height, JScrollPane posX, JScrollPane posY, arcX, arcY
-    Integer[] content_panel = {100, 80, 715, 650, 10, 10, 695, 630, 20, 20};
-        //format as follows: width, and height
-        Integer[] main_content_holder = {695,1000};
-        // format as follows: height, width, button wh, button ht.
-        Integer[] sub_panel_sizes= {882, 50, 50, 50};
+    // format as follows: posX, posY, width, height, JScrollPane posX, JScrollPane posY JScrollPane height, JScrollPane width, arcX, arcY.
+    private Integer[] side_bar_panel = {10, 80, 80, 650, 5, 5, 70, 630, 20, 20};
+        // format as follows: height, width
+        private Integer[] sub_panel_sizes= {882, 50};
+        // format as follows: posX, posY, width, height
+        private Integer[] library_button = {9, 5, 50, 50};
+        // format as follows: posX, width, height
+        private Integer[] side_bar_button = {9, 50, 50};
+        // format as follows: posX, posY, width, height, JScrollPane posX, JScrollPane posY, JScrollPane width, JScrollPane height, arcX, arcY 
+        
+    private Integer[] content_panel = {100, 80, 715, 650, 10, 10, 695, 630, 20, 20};
+    //format as follows: width, and height
+        private Integer[] main_content_holder = {0, 1080};
         // array list as follows: posX, posY, width, height, gridLayout_row, gridLayout_column, grid_hgap, grid_vgap
-        Integer[] sub_top_panel = {1, 1, 694, 130, 2, 4, 5, 5};
+        private Integer[] sub_top_panel = {1, 1, 694, 130, 2, 4, 5, 5};
+
+        // outer panel that holds the entire for loop, as follows: posX, width, height
+        private Integer[] panel_object = {0, 694, 180};
+            // inner title text, as follows: posX, posY, width, height, font size
+            private Integer[] main_title = {1, 10, 500, 30, 24};
+            // panel that holds the album/singer, as follows: posX, posY, width, height, rows, column, hgap, vgap.
+            private Integer[] list_holder = {1, 40, 694, 140, 0, 4, 10, 0};
+            // the album/singer itself, as follows: width, height, arcWidth, arcHeight
+            private Integer[] pic_holder = {100, 100, 20, 20};
+
+    SuperMain(){
     
+    frame = new Frame(null);
 
     Panel headerPanel = new Panel(Color.BLUE, header_panel[0], header_panel[1], header_panel[2], header_panel[3], null);
     MouseListener mouse = new MouseListener(); 
@@ -58,10 +80,6 @@ class SuperMain{
     headerPanel.addMouseListener(mouse);
     headerPanel.addMouseMotionListener(mouse);
     frame.add(headerPanel);
-
-        // Panel searchBarPanel = new Panel(Color.WHITE, searchbar_value[0], searchbar_value[1], searchbar_value[2], searchbar_value[3], null);
-        // searchBarPanel.setOpaque(false);
-        // headerPanel.add(searchBarPanel);
 
         Button minimizeButton = new Button(minimize_value[0], minimize_value[1], minimize_value[2], minimize_value[3]);
         minimizeButton.setOpaque(false);
@@ -100,7 +118,7 @@ class SuperMain{
         });
         headerPanel.add(closeButton);
 
-    Panel sidebarPanel = new Panel(Color.DARK_GRAY, side_bar_panel[0], side_bar_panel[1], side_bar_panel[2], side_bar_panel[3], null){
+    sidebarPanel = new Panel(Color.DARK_GRAY, side_bar_panel[0], side_bar_panel[1], side_bar_panel[2], side_bar_panel[3], null){
         @Override
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
@@ -121,17 +139,20 @@ class SuperMain{
     frame.add(sidebarPanel);
 
             //Panel that holds the scrollable area in the sidebar.
-            Panel subPanel = new Panel(Color.DARK_GRAY, sub_panel_sizes[0], sub_panel_sizes[1], new FlowLayout(FlowLayout.CENTER));
+            Panel subPanel = new Panel(Color.BLACK, sub_panel_sizes[0], sub_panel_sizes[1], null);
             subPanel.setOpaque(true);
+                libraryButton = new Button(library_button[0], library_button[1], library_button[2], library_button[3]);
+                libraryButton.addActionListener(this);
+                subPanel.add(libraryButton);
 
-                Button[] object = new Button[16];
-                for(int i = 0; i < 16; i++){
-                    object[i] = new Button(sub_panel_sizes[2], sub_panel_sizes[3]);
+                object = new Button[15];
+                for(int i = 0; i < 15; i++){
+                    object[i] = new Button(side_bar_button[0], 60 + (i * 55) ,side_bar_button[1], side_bar_button[2]);
                     object[i].setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                     subPanel.add(object[i]);
                 }
 
-            JScrollPane scroll = new JScrollPane(subPanel);
+            scroll = new JScrollPane(subPanel);
             scroll.getVerticalScrollBar().setUnitIncrement(5);
             scroll.setBounds(side_bar_panel[4], side_bar_panel[5], side_bar_panel[6], side_bar_panel[7]);
             scroll.setBorder(BorderFactory.createEmptyBorder());
@@ -141,7 +162,7 @@ class SuperMain{
         
 
         //Color, posX, posY, width, height
-    Panel contentPanel = new Panel(Color.DARK_GRAY, content_panel[0], content_panel[1], content_panel[2], content_panel[3], null){
+    contentPanel = new Panel(Color.DARK_GRAY, content_panel[0], content_panel[1], content_panel[2], content_panel[3], null){
         @Override
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
@@ -168,7 +189,7 @@ class SuperMain{
     mainContentHolder.setBackground(Color.DARK_GRAY);
     mainContentHolder.setPreferredSize(new Dimension(main_content_holder[0], main_content_holder[1]));
 
-            Panel subTopPanel = new Panel(null, sub_top_panel[0], sub_top_panel[1], sub_top_panel[2], sub_top_panel[3], new GridLayout(sub_top_panel[4],sub_top_panel[5],sub_top_panel[6],sub_top_panel[7]));
+            subTopPanel = new Panel(null, sub_top_panel[0], sub_top_panel[1], sub_top_panel[2], sub_top_panel[3], new GridLayout(sub_top_panel[4],sub_top_panel[5],sub_top_panel[6],sub_top_panel[7]));
             // grid layout set on a 2x4 rowXcolumn with a set size
                 Color[] color_array = {Color.BLACK, Color.RED, Color.GREEN, Color.WHITE, Color.CYAN, Color.MAGENTA, Color.PINK, Color.YELLOW};
                 Panel[] playlist = new Panel[8];
@@ -205,54 +226,51 @@ class SuperMain{
     mainContentScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     mainContentScroll.setBounds(content_panel[4], content_panel[5], content_panel[6], content_panel[7]);
     contentPanel.add(mainContentScroll);
-
-    //put nested album/singer panels on a gridLayout with a row of 0 and column of sumn     
-
-    // as follows: panel#, mainTitle#, 
-
-    Panel[] panelObject = new Panel[4];
-    String[] label_text = {"Recently Played", "Made for you", "Your Favorite Artist", "Jump Back In"};
-    Label[] mainTitle = new Label[4];
-    Panel[] listHolder = new Panel[4];
-    Panel[] picHolder = new Panel[4];
-
-    for (int i = 0; i < panelObject.length; i++){
-        panelObject[i] = new Panel(Color.RED, 0, (30 + 130) + (i * 210), 694, 180, null);
-        //posY 30 is the spacing between four panels, 130 is the height of upper outer panel of the content, 210 is the total height of the panel combined with pre existing (or not) spacing... im so proud of myself XD      
-        // panelObject[i].setOpaque(true);
-        mainContentHolder.add(panelObject[i]);
-
-        mainTitle[i] = new Label(label_text[i], "Myanmar Text", Color.WHITE, Font.BOLD, 10, 10, 500, 30, 24);
-        panelObject[i].add(mainTitle[i]);
-
-        listHolder[i] = new Panel(Color.BLUE, 0, 40, 694, 140, new GridLayout(0, 4, 10, 0));
-        // listHolder[i].setOpaque(true);
-        panelObject[i].add(listHolder[i]);
-
-        for (int j = 0; j < picHolder.length; j++){
-            picHolder[i] = new Panel(Color.ORANGE, 100, 100, null){
-                @Override
-                protected void paintComponent(Graphics g){
-                    super.paintComponent(g);
-                    Dimension arcs = new Dimension(20, 20);
-                    int width = getWidth();
-                    int height = getHeight();
-                    Graphics2D g2d = (Graphics2D)g;
-                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     
-                    //Draws the rounded opaque panel with borders
-                    g2d.setColor(getBackground());
-                    g2d.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
-                    g2d.setColor(getForeground());
-                    g2d.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+    
+
+    Panel[] panelObject = new Panel[4]; //4 outer panel that holds the mainTitle, listHolder, and picsHolder.
+    Label[] mainTitle = new Label[4]; //4 inner specific Title on each panelObject.
+    listHolder = new Panel[4]; //inner panel that holds the album together.
+    Panel[] picHolder = new Panel[4]; //album itself.
+
+            for (int i = 0; i < panelObject.length; i++){
+                panelObject[i] = new Panel(Color.RED, panel_object[0], (60 + 130) + (i * 230), panel_object[1], panel_object[2], null);
+                //posY 30 is the spacing between (4) panels, 130 is the height of upper outer panel of the content, 210 is the total height of the panel combined with pre existing (or not) spacing... im so proud of myself XD      
+                // panelObject[i].setOpaque(true);
+                mainContentHolder.add(panelObject[i]);
+
+                mainTitle[i] = new Label(data.getTitle(i), "Myanmar Text", Color.WHITE, Font.BOLD, main_title[0], main_title[1], main_title[2], main_title[3], main_title[4]);
+                panelObject[i].add(mainTitle[i]);
+
+                listHolder[i] = new Panel(Color.BLUE, list_holder[0], list_holder[1], list_holder[2], list_holder[3], new GridLayout(list_holder[4], list_holder[5], list_holder[6] , list_holder[7]));
+                // listHolder[i].setOpaque(true);
+                panelObject[i].add(listHolder[i]);
+
+                for (int j = 0; j < picHolder.length; j++){
+                    picHolder[i] = new Panel(Color.ORANGE, pic_holder[0], pic_holder[1], null){
+                        @Override
+                        protected void paintComponent(Graphics g){
+                            super.paintComponent(g);
+                            Dimension arcs = new Dimension(pic_holder[2], pic_holder[3]);
+                            int width = getWidth();
+                            int height = getHeight();
+                            Graphics2D g2d = (Graphics2D)g;
+                            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            
+                            //Draws the rounded opaque panel with borders
+                            g2d.setColor(getBackground());
+                            g2d.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+                            g2d.setColor(getForeground());
+                            g2d.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height);
+                        }
+                    };
+                    // picHolder[i].setOpaque(true);
+                    listHolder[i].add(picHolder[i]);
                 }
             };
-            // picHolder[i].setOpaque(true);
-            listHolder[i].add(picHolder[i]);
-        }
-    };
     frame.setVisible(true);
-}
+    }
 
     public class MouseListener extends MouseAdapter{
         private Point mouse_rel_comp_coords = null;
@@ -268,13 +286,73 @@ class SuperMain{
         }
     }
 
-    public static void libraryClicked(){
-        
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == libraryButton){
+
+            is_toggled = !is_toggled;
+            if (is_toggled){
+                sidebarPanel.setBounds(side_bar_panel[0], side_bar_panel[1], 255, side_bar_panel[3]);
+                sidebarPanel.revalidate();
+                sidebarPanel.repaint();
+                scroll.setBounds(side_bar_panel[4], side_bar_panel[5], 240, side_bar_panel[7]);
+                scroll.revalidate();
+                scroll.repaint();
+                for (int i = 0; i < object.length; i++){
+                    object[i].setBounds(side_bar_button[0], 60 + (i * 55), 220, side_bar_button[2]);
+                    object[i].revalidate();
+                    object[i].repaint();
+                }
+                libraryButton.setBounds(library_button[0], library_button[1], 220, library_button[3]);
+                libraryButton.revalidate();
+                libraryButton.repaint();
+                contentPanel.setBounds(275, content_panel[1], 540, content_panel[3]);
+                contentPanel.revalidate();
+                contentPanel.repaint();
+                subTopPanel.setBounds(sub_top_panel[0], sub_top_panel[1], 524, sub_top_panel[3]);
+                subTopPanel.revalidate();
+                subTopPanel.repaint();
+                for (int i = 0; i < listHolder.length; i++){
+                    listHolder[i].setBounds(list_holder[0], list_holder[1], 524, list_holder[3]);
+                    listHolder[i].setLayout(new GridLayout(list_holder[4], list_holder[5], 5, list_holder[7]));
+                    listHolder[i].revalidate();
+                    listHolder[i].repaint();
+                }
+            }
+            else if (!is_toggled) {
+                sidebarPanel.setBounds(side_bar_panel[0], side_bar_panel[1], side_bar_panel[2], side_bar_panel[3]);
+                sidebarPanel.revalidate();
+                sidebarPanel.repaint();
+                scroll.setBounds(side_bar_panel[4], side_bar_panel[5], side_bar_panel[6], side_bar_panel[7]);
+                scroll.revalidate();
+                scroll.repaint();
+                for (int i = 0; i < object.length; i++){
+                    object[i].setBounds(side_bar_button[0], 60 + (i * 55), side_bar_button[1], side_bar_button[2]);
+                    object[i].revalidate();
+                    object[i].repaint();
+                }
+                libraryButton.setBounds(library_button[0], library_button[1], library_button[2], library_button[3]);
+                libraryButton.revalidate();
+                libraryButton.repaint();
+                contentPanel.setBounds(content_panel[0], content_panel[1], content_panel[2], content_panel[3]);
+                contentPanel.revalidate();
+                contentPanel.repaint();
+                subTopPanel.setBounds(sub_top_panel[0], sub_top_panel[1], sub_top_panel[2], sub_top_panel[3]);
+                subTopPanel.revalidate();
+                subTopPanel.repaint();
+                for (int i = 0; i < listHolder.length; i++){
+                    listHolder[i].setBounds(list_holder[0], list_holder[1], list_holder[2], list_holder[3]);
+                    listHolder[i].setLayout(new GridLayout(list_holder[4], list_holder[5], list_holder[6], list_holder[7]));
+                    listHolder[i].revalidate();
+                    listHolder[i].repaint();
+                }
+            }
+        }
     }
 
 }
 
-// custom jswing
+// custom jswing ----------------------------------------------------------------------------------------
 
 class Button extends JButton{
     Button(int posX, int posY, int width, int height){
