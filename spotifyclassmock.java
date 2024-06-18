d798585c87dd6102ac2dfa1390451c7a7baede46
echoxxkeeper_ <147575793+echoxxkeeper@users.noreply.github.com>
2024-06-18 10:48:00 +0800

    Update spotifyclassmock.java

--- spotifyclassmock.java
+++ spotifyclassmock.java
@@ -1,24 +1,6 @@
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.LayoutManager;
-import java.awt.Point;
-import java.awt.RenderingHints;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 class SuperMain implements ActionListener{
 
